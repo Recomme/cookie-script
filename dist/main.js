934b43a2c1ef40ce6c93ea15f67dfeca60e6746c
Grzegorz Biernacki <grzegorz.biernacki@recom.pl>
2021-03-21 13:59:55 +0100

    Zapis rcr z urla do cookiesa

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-!function(){let e=window.location.hostname,t="."+e.substring(e.lastIndexOf(".",e.lastIndexOf(".")-1)+1),n=new Date;n.setMonth(n.getMonth()+1),document.cookie="recomme_r_code=abc123567;expires="+n+";domain="+t+";path=/"}();
+!function(){let e=window.location.hostname,t="."+e.substring(e.lastIndexOf(".",e.lastIndexOf(".")-1)+1),n=new Date;n.setMonth(n.getMonth()+1);let o=new URL(window.location.href).searchParams.get("rcr");document.cookie="recomme_r_code="+o+";expires="+n+";domain="+t+";path=/"}();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 (function() {
   let hostname = window.location.hostname;
-  let domain = "." + hostname.substring(hostname.lastIndexOf(".", hostname.lastIndexOf(".") - 1) + 1);
-  let cookieName = 'recomme_r_code';
-  let cookieValue = 'abc123567';
-  let myDate = new Date();
-  myDate.setMonth(myDate.getMonth() + 1);
+  let cookieDomain = "." + hostname.substring(hostname.lastIndexOf(".", hostname.lastIndexOf(".") - 1) + 1);
+  let cookieName = "recomme_r_code";
+  
+  let cookieDate = new Date();
+  cookieDate.setMonth(cookieDate.getMonth() + 1);
 
-  document.cookie = cookieName +"=" + cookieValue + ";expires=" + myDate 
-                    + ";domain="+domain+";path=/";
+  let url = new URL(window.location.href);
+  let rcrCode = url.searchParams.get("rcr");
+  let cookieValue = rcrCode;
+
+  document.cookie = cookieName +"=" + cookieValue + ";expires=" + cookieDate 
+                    + ";domain="+cookieDomain+";path=/";
 })();
